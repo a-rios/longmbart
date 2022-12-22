5fe351df8aa6c581bde46833484cc3a88427a5da
Lukas <fischerl@cl.uzh.ch>
2022-12-22 11:15:47 +0100

    remove unnecessary arguments

--- longformer/score.py
+++ longformer/score.py
@@ -39,50 +39,6 @@ from longformer.simplification import prepare_input, get_eval_scores, remove_spe
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
-
-# class SimplificationDatasetForInference(Dataset):
-#     def __init__(self, inputs, reference, name, tokenizer, max_input_len, max_output_len, src_lang, tgt_lang, tags_included, target_tags):
-#         self.inputs = inputs
-#         self.reference = reference
-#         self.name = name # train, val, test
-#         self.tokenizer = tokenizer
-#         self.max_input_len = max_input_len
-#         self.max_output_len = max_output_len
-#         self.src_lang = src_lang
-#         self.tgt_lang = tgt_lang
-#         self.tags_included = tags_included
-#         self.target_tags = target_tags
-#
-#     def __len__(self):
-#         return len(self.inputs)
-#
-#     def __getitem__(self, idx):
-#         source = self.inputs[idx]['text']
-#         reference = None
-#         target_tags = None
-#         if self.reference is not None:
-#             reference = self.reference[idx]['text']
-#         if self.target_tags is not None:
-#             target_tags = self.target_tags[idx]['text']
-#         if self.tags_included is not None:
-#             sample = self.tokenizer.prepare_seq2seq_batch(src_texts=[source], tags_included=True , max_length=self.max_input_len, max_target_length=self.max_output_len, truncation=True, padding=False, return_tensors="pt")
-#         else:
-#             sample = self.tokenizer.prepare_seq2seq_batch(src_texts=[source], src_lang=self.src_lang, tgt_lang=self.tgt_lang , max_length=self.max_input_len, max_target_length=self.max_output_len, truncation=True, padding=False, return_tensors="pt") # TODO move this to _get_dataloader, preprocess everything at once?
-#
-#         input_ids = sample['input_ids'].squeeze()
-#         input_ids = torch.roll(input_ids, shifts=-1) # move language tag to the end of the sequence in source
-#         return input_ids, reference, target_tags
-#
-#     @staticmethod
-#     def collate_fn(batch):
-#
-#         pad_token_id = 1
-#
-#         input_ids, ref, target_tags = list(zip(*batch))
-#         input_ids = torch.nn.utils.rnn.pad_sequence(input_ids, batch_first=True, padding_value=pad_token_id)
-#         return input_ids, ref, target_tags
-
-
 class SimplifierScorer(InferenceSimplifier):
 
     def __init__(self, params):
@@ -102,7 +58,7 @@ class SimplifierScorer(InferenceSimplifier):
         sampler = torch.utils.data.distributed.DistributedSampler(dataset,
                                                                   shuffle=is_train) if self.trainer.use_ddp else None
 
-        return DataLoader(dataset, batch_size=1, shuffle=(sampler is None),
+        return DataLoader(dataset, batch_size=10, shuffle=(sampler is None),
                           num_workers=self.args.num_workers, sampler=sampler,
                           collate_fn=SimplificationDataset.collate_fn)
 
@@ -151,43 +107,11 @@ class SimplifierScorer(InferenceSimplifier):
         logging.debug("loss:", loss, "source:", source_strs, "target:", target_strs)
 
         with open(self.args.output, 'a') as f:
-             f.write(str(loss) + "\n")
-
-        # generated_strs = []
-        #
-        # for batch_i in range(len(batch_source_strs)):
-        #     src_str = batch_source_strs[batch_i]
-        #     if self.args.test_target:
-        #         ref_str = ' '.join(ref[batch_i].split(' ')[1:]) if self.tags_included else ref[batch_i]
-        #     else:
-        #         ref_str = None
-        #
-        #     # subselect only those hyps/scores for the
-        #     # relevant src string
-        #     hyps = batch_hyp_strs[batch_i:batch_i + self.args.num_return_sequences]
-        #     scores = batch_hyp_scores[batch_i:batch_i + self.args.num_return_sequences]
-        #
-        #     output_dict = {
-        #         'src': src_str,
-        #         'ref': ref_str,
-        #         'hyps': [],
-        #     }
-        #     # Ensure output hyps are sorted by
-        #     # overall NLL probability scores (smaller = better).
-        #     scored_hyps = {score: hyp for score, hyp in zip(scores, hyps)}
-        #     for i, score in enumerate(sorted(scored_hyps.keys(), reverse=True)):
-        #         # add the 1-best hypothesis to generated_strs for evaluation
-        #         if i == 0:
-        #             generated_strs.append(scored_hyps[score])
-        #         output_dict['hyps'].append({'score': score, 'hyp': scored_hyps[score]})
-        #
-        #     json_line = json.dumps(output_dict, ensure_ascii=False)
-        #     with open(self.args.translation, 'a') as f:
-        #         f.write(json_line + "\n")
+             f.write(str(loss.item()) + "\n")
 
 
     def test_epoch_end(self, outputs):
-        print(outputs)
+        pass
 
     @staticmethod
     def add_model_specific_args(parser, root_dir):
@@ -200,64 +124,23 @@ class SimplifierScorer(InferenceSimplifier):
         parser.add_argument("--test_source", type=str, default=None, help="Path to the source test file.")
         parser.add_argument("--test_target", type=str, default=None,
                             help="Path to the target test file (optional, if given, will output rouge and bleu).")
-        parser.add_argument("--target_tags", type=str, default=None,
-                            help="If test_target is not given: provide path to file with list of target tags (one per sample in test_source).")
         parser.add_argument("--src_lang", type=str, default=None,
                             help="Source language tag (optional, for multilingual batches, preprocess text files to include language tags.")
         parser.add_argument("--tgt_lang", type=str, default=None,
                             help="Target language tag (optional, for multilingual batches, preprocess text files to include language tags.")
         parser.add_argument("--tags_included", action='store_true',
                             help="Text files already contain special tokens (language tags and </s>. Source:  src_tag seq, Target:  tgt_tag seq. Note: actual source sequence is seq src_tag </s>, will be changed internally after possibly clipping sequence to given max_length.")
-        parser.add_argument("--infer_target_tags", action="store_true", default=False,
-                            help="If test_target is not given and target language tags can be inferred from the source language tags provided with --tags_included (e.g. de_DE -> de_DE). This save having a dedicated text file in which the tags are explicitly specified.")
         parser.add_argument("--max_input_len", type=int, default=256,
                             help="maximum num of wordpieces, if unspecified, will use number of encoder positions from model config.")
         parser.add_argument("--max_output_len", type=int, default=512,
                             help="maximum num of wordpieces, if unspecified, will use number of decoder positions from model config.")
-        parser.add_argument("--bad_words", type=str, default=None, help="Path to file containing bad words.")
-        parser.add_argument("--used_bad_words", type=str, default=None,
-                            help="Path to file where used bad words should be saved.")
-        parser.add_argument("--remove_special_tokens_containing", type=str, nargs="+",
-                            help="Remove tokens from the special_tokens_map that contain this string")
-        # TODO
-        # parser.add_argument("--do_predict", action="store_true", default=False, help="If given, predictions are run using the `predict_step()` method rather than `test_step()`. Outputs are written to the specified output file without being evaluated!")
 
         parser.add_argument("--num_workers", type=int, default=0, help="Number of data loader workers")
         parser.add_argument("--gpus", type=int, default=-1, help="Number of gpus. 0 for CPU")
 
-        ## inference params
-        parser.add_argument("--translation", type=str, default='decoded.out',
-                            help="Output file to write decoded sequence to.")
-        parser.add_argument("--beam_size", type=int, default=4,
-                            help="Beam size for inference when testing/validating. Default: 4.")
-        parser.add_argument("--test_percent_check", default=1.00, type=float, help='Percent of test data used')
-        parser.add_argument("--global_attention_indices", type=int, nargs='+', default=[-1], required=False,
-                            help="List of indices of positions with global attention for longformer attention. Supports negative indices (-1 == last non-padding token). Default: [-1] == last source token (==lang_id) .")
-        parser.add_argument("--keep_special_tokens", default=False, action="store_true",
-                            help="If true, do not skip special tokens.")
-
+        #TODO ??
         parser.add_argument("--output_to_json", default=False, action="store_true",
                             help='If true, decoding output is a verbose JSONL containing, src, tgt, and scored model output hyps')
-
-        # decoding strategy params (passed to model.generate() (in generation_utils.py))
-        parser.add_argument("--do_sample", default=False, action="store_true",
-                            help='Whether or not to use sampling ; use greedy decoding otherwise.')
-        parser.add_argument("--temperature", default=1.0, type=float,
-                            help='The value used to module the next token probabilities.')
-        parser.add_argument("--top_k", default=50, type=int,
-                            help='The number of highest probability vocabulary tokens to keep for top-k-filtering.')
-        parser.add_argument("--top_p", default=1.0, type=float,
-                            help='If set to float < 1, only the most probable tokens with probabilities that add up to :obj:`top_p` or higher are kept for generation.')
-        parser.add_argument("--repetition_penalty", default=1.0, type=float,
-                            help='The parameter for repetition penalty. 1.0 means no penalty.')
-        parser.add_argument("--length_penalty", default=1.0, type=float,
-                            help='Exponential penalty to the length. 1.0 means no penalty.')
-        parser.add_argument("--output_scores", default=False, action="store_true",
-                            help='Whether or not to return the prediction scores.')
-        parser.add_argument("--num_return_sequences", default=1, type=int,
-                            help='The number of independently computed returned sequences for each element in the batch, i.e. N-best')
-        parser.add_argument("--return_dict_in_generate", default=False, action="store_true",
-                            help='Whether or not to return a :class:`~transformers.file_utils.ModelOutput` instead of a plain tuple.')
 
         # logging params
         parser.add_argument("--progress_bar_refresh_rate", type=int, default=0,
